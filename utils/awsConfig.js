2ec7d4017153b04b44e19b0af9f16431854e02d2
rudrameher <rudra@maxdrive.ai>
2022-11-17 19:48:24 +0530

    added readmemd

--- utils/awsConfig.js
+++ utils/awsConfig.js
@@ -1,10 +1,10 @@
 export default {
   awsEndpoint: "",
   topics: {
-    contract: process.env.AWS_TOPIC_ARN_CONTRACT,
-    champion: process.env.AWS_TOPIC_ARN_CHAMPION
+    contract: process?.env?.AWS_TOPIC_ARN_CONTRACT,
+    champion: process?.env?.AWS_TOPIC_ARN_CHAMPION
   },
-  awsRegion: process.env.AWS_REGION
+  awsRegion: process?.env?.AWS_REGION
 };
 
 
